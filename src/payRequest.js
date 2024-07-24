f00741ab8f3f3e49c2b74b2f7b49ac48dbcfb6c5
MantisClone <david.huntmateo@request.network>
2024-07-23 22:05:39 -0400

    Update hasSufficientFunds function call

--- src/payRequest.js
+++ src/payRequest.js
@@ -88,13 +88,11 @@
   console.log(
     `Checking if payer ${payerWallet.address} has sufficient funds...`,
   );
-  const _hasSufficientFunds = await hasSufficientFunds(
-    requestData,
-    payerWallet.address,
-    {
-      provider: provider,
-    },
-  );
+  const _hasSufficientFunds = await hasSufficientFunds({
+    request: requestData,
+    address: payerWallet.address,
+    providerOptions: { provider: provider },
+  });
   console.log(`_hasSufficientFunds = ${_hasSufficientFunds}`);
   if (!_hasSufficientFunds) {
     throw new Error(`Insufficient Funds: ${payerWallet.address}`);
