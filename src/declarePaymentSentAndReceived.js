61c1fb33f92b666fdf97297b5be8c1266ddc5179
MantisClone <david.huntmateo@request.network>
2024-08-21 16:16:06 -0400

    Clean up process exception handlers. Never saw them work

--- src/declarePaymentSentAndReceived.js
+++ src/declarePaymentSentAndReceived.js
@@ -6,14 +6,6 @@ const waitForConfirmation = async (dataOrPromise) => {
   });
 };
 
-process.on("uncaughtException", (err) => {
-  console.error(err);
-  process.exit(1);
-});
-process.on("unhandledRejection", (err) => {
-  console.error(err);
-  process.exit(1);
-});
 (async () => {
   const {
     RequestNetwork,
