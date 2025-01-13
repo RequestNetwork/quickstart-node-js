01915239f5921833f07817e83a2bd5fbfbf8243b
MantisClone <david.huntmateo@request.network>
2025-01-13 16:56:31 -0500

    chore: add comments clarifying that script doesn't actually work

--- src/depositToHinkalShieldedAddress.js
+++ src/depositToHinkalShieldedAddress.js
@@ -4,7 +4,10 @@ import { providers } from "ethers";
 (async () => {
   const RPC_URL = "https://mainnet.base.org";
   const provider = new providers.JsonRpcProvider(RPC_URL);
+  // FIXME: This script won't actually work because a random wallet won't have any funds to deposit
   const paymentSender = wallet.createRandom(provider);
+  // Hinkal shielded addresses must be shared out-of-band.
+  // The RN SDK doesn't offer functions for sharing Hinkal shielded addresses.
   const paymentRecipientShieldedAddress =
     "142590100039484718476239190022599206250779986428210948946438848754146776167,0x096d6d5d8b2292aa52e57123a58fc4d5f3d66171acd895f22ce1a5b16ac51b9e,0xc025ccc6ef46399da52763a866a3a10d2eade509af27eb8411c5d251eb8cd34d";
   await sendToHinkalShieldedAddressFromPublic({
